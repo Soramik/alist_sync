49b41aa0a382813ecb728c253adfdea410786faa
Soramik <1243510748@qq.com>
2022-10-06 21:42:27 +0800

    log

--- common/log.py
+++ common/log.py
@@ -14,26 +14,30 @@ from logging.handlers import RotatingFileHandler
 
 import colorlog
 
+cur_path = os.path.dirname(os.path.realpath(__file__))  # 当前项目路径
+
 # 日志设置(只影响终端, 不影响日志文件)
 if platform.system() == 'Windows':
     LOG_LEVEL = logging.DEBUG
+    LOG_PATH = os.path.join(os.path.dirname(cur_path), 'logs')
 else:
     LOG_LEVEL = logging.INFO
+    LOG_PATH = '~/sync/logs'
+
+# 是否输出文件
+write_log_flag = False
 
 
 class BaseLog:
-    cur_path = os.path.dirname(os.path.realpath(__file__))  # 当前项目路径
-    if platform.system() == 'Windows':
-        log_path = os.path.join(os.path.dirname(cur_path), 'logs')  # log_path为存放日志的路径
-    else:
-        log_path = "/home/john/logs"  # log_path为存放日志的路径
-    try:
-        if not os.path.exists(log_path):
-            os.mkdir(log_path)  # 若不存在logs文件夹，则自动创建
-    except FileNotFoundError:
-        log_path = os.path.join(os.path.dirname(cur_path), 'logs')  # log_path为存放日志的路径
-        if not os.path.exists(log_path):
-            os.mkdir(log_path)  # 若不存在logs文件夹，则自动创建
+    if write_log_flag is True:
+        log_path = LOG_PATH  # log_path为存放日志的路径
+        try:
+            if not os.path.exists(log_path):
+                os.mkdir(log_path)  # 若不存在logs文件夹，则自动创建
+        except FileNotFoundError:
+            log_path = os.path.join(os.path.dirname(cur_path), 'logs')  # log_path为存放日志的路径
+            if not os.path.exists(log_path):
+                os.mkdir(log_path)  # 若不存在logs文件夹，则自动创建
     log_colors_config = {
         # 终端输出日志颜色配置
         'DEBUG': 'white',
@@ -138,76 +142,30 @@ class MainLog(BaseLog):
             loglevel = logging.DEBUG
         super().__init__()
         self.__now_time = datetime.now().strftime('%Y-%m-%d')  # 当前日期格式化
-        self.__all_log_path = os.path.join(self.log_path, self.__now_time + "-all" + ".log")  # 收集所有日志信息文件
-        self.__error_log_path = os.path.join(self.log_path, self.__now_time + "-error" + ".log")  # 收集错误日志信息文件
+        if write_log_flag is True:
+            self.__all_log_path = os.path.join(self.log_path, self.__now_time + "-all" + ".log")  # 收集所有日志信息文件
+            self.__error_log_path = os.path.join(self.log_path, self.__now_time + "-error" + ".log")  # 收集错误日志信息文件
         self.LOG_LEVEL = loglevel
-
-    def __console(self, level, message):
-        """构造日志收集器"""
-        # 创建日志文件
-        all_logger_handler = self._init_logger_handler(self.__all_log_path)  # 收集所有日志文件
-        error_logger_handler = self._init_logger_handler(self.__error_log_path)  # 收集错误日志信息文件
-        console_handle = self._init_console_handle()
-
-        # 设置日志文件格式
-        self._set_log_formatter(all_logger_handler)
-        self._set_log_formatter(error_logger_handler)
-        self._set_color_formatter(console_handle, self.log_colors_config)
-
-        self._set_log_handler(all_logger_handler)  # 设置handler级别并添加到logger收集器
-        self._set_log_handler(error_logger_handler, level=logging.ERROR)
-        self._set_color_handle(console_handle)
-
-        if level == 'info':
-            self._logger.info(message)
-        elif level == 'debug':
-            self._logger.debug(message)
-        elif level == 'warning':
-            self._logger.warning(message)
-        elif level == 'error':
-            self._logger.error(message, exc_info=True, stack_info=True)
-            # exc_info=True, stack_info=True 用于在日志中记录堆栈信息，方便查看日志进行调试
-        elif level == 'critical':
-            self._logger.critical(message, exc_info=True, stack_info=True)
-            # exc_info=True, stack_info=True 用于在日志中记录堆栈信息，方便查看日志进行调试
-
-        self._logger.removeHandler(all_logger_handler)  # 避免日志输出重复问题
-        self._logger.removeHandler(error_logger_handler)
-        self._logger.removeHandler(console_handle)
-
-        self._close_handler(all_logger_handler)  # 关闭handler
-        self._close_handler(error_logger_handler)
-
-    def debug(self, message):
-        self.__console('debug', message)
-
-    def info(self, message):
-        self.__console('info', message)
-
-    def warning(self, message):
-        self.__console('warning', message)
-
-    def error(self, message):
-        self.__console('error', message)
-
-    def critical(self, message):
-        self.__console('critical', message)
 
     def __call__(self):
         """构造日志收集器"""
-        # 创建日志文件
-        all_logger_handler = self._init_logger_handler(self.__all_log_path)
-        error_logger_handler = self._init_logger_handler(self.__error_log_path)
+
+        if write_log_flag is True:
+            # 创建日志文件
+            all_logger_handler = self._init_logger_handler(self.__all_log_path)
+            error_logger_handler = self._init_logger_handler(self.__error_log_path)
+            # 设置日志格式
+            self._set_log_formatter(all_logger_handler)
+            self._set_log_formatter(error_logger_handler)
+            # 设置handler级别并添加到logger收集器
+            self._set_log_handler(all_logger_handler, level=logging.DEBUG)
+            self._set_log_handler(error_logger_handler, level=logging.ERROR)
+
+        # 创建终端日志输出
         console_handle = self._init_console_handle()
-
         # 设置日志格式
-        self._set_log_formatter(all_logger_handler)
-        self._set_log_formatter(error_logger_handler)
         self._set_color_formatter(console_handle, self.log_colors_config)
-
         # 设置handler级别并添加到logger收集器
-        self._set_log_handler(all_logger_handler, level=logging.DEBUG)
-        self._set_log_handler(error_logger_handler, level=logging.ERROR)
         self._set_color_handle(console_handle, level=self.LOG_LEVEL)
 
         return self._logger
